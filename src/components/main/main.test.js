d042aefd99fe818356e5a42b37c48c233c4bc04e
Юлия <orlova.yuliia.v@gmail.com>
2020-02-12 16:41:43 +0300

    2.2 Добавляет снепшот тесты

---
+++ src/components/main/main.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const PromoData = {
+  PROMO_NAME: `The Grand Budapest Hotel`,
+  PROMO_GENRE: `Drama`,
+  PROMO_YEAR: 2014
+};
+const filmsTitle = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`];
+
+it(`should render correctly`, () => {
+  const tree = renderer
+   .create(<Main
+     promoName={PromoData.PROMO_NAME}
+     promoGenre={PromoData.PROMO_GENRE}
+     promoYear={PromoData.PROMO_YEAR}
+     filmsTitle={filmsTitle}
+   />)
+   .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
