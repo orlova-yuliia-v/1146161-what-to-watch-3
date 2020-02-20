95af0ff10fca11cd4a08c4dece951ca1d6ff5b8b
Юлия <orlova.yuliia.v@gmail.com>
2020-02-20 21:21:35 +0300

    3.2 Настраивает роутинг в App-классовом компоненте

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
+import MovieDetails from "../movie-details/movie-details.jsx";
 
 const movieTitleClickHandler = () => {};
 
-const App = (props) => {
-  const {promoTitle, promoGenre, promoYear, films} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <Main
-      promoTitle={promoTitle}
-      promoGenre={promoGenre}
-      promoYear={promoYear}
-      onMovieTitleClick={movieTitleClickHandler}
-      films={films} />
-  );
-};
+  render() {
+    const {promoTitle, promoGenre, promoYear, films} = this.props;
+
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              promoTitle={promoTitle}
+              promoGenre={promoGenre}
+              promoYear={promoYear}
+              onMovieTitleClick={movieTitleClickHandler}
+              films={films} />
+          </Route>
+          <Route exact path="/dev-film-details">
+            <MovieDetails />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   promoTitle: PropTypes.string.isRequired,
