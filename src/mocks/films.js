dfd76d5af9651c0807f1ccf92586c4c962d0e46e
Юлия <orlova.yuliia.v@gmail.com>
2020-02-21 10:35:44 +0300

    3.2 Обновляет моки (добавляет детальную информацию)

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,34 +1,114 @@
 export default [
   {
     title: `Fantastic Beasts: The Crimes of Grindelwal`,
-    poster: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    poster: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    bgPosterUrl: `https://d2eib6r9tuf5y8.cloudfront.net/l/assets/img/article/article-2491-kqk17iex/keyvisual.jpg`,
+    genre: `Kids & Family`,
+    releaseYear: 2018,
+    director: `David Yates`,
+    starring: [`Johnny Depp`, `Kevin Guthrie`, `Eddie Redmayne`, `Katherine Waterston`, `Dan Fogler`],
+    runTime: `2h 14m`,
+    ratingScore: `6,6`,
+    ratingLevel: `Good`,
+    ratingCount: 1390,
+    description: `The second installment of the "Fantastic Beasts" series featuring the adventures of Magizoologist Newt Scamander`
   },
   {
     title: `Bohemian Rhapsody`,
-    poster: `img/bohemian-rhapsody.jpg`
+    poster: `img/bohemian-rhapsody.jpg`,
+    bgPosterUrl: `https://www.quicksilvertranslate.com/wp-content/uploads/bohemian-rhapsody-1.jpg`,
+    genre: `Dramas`,
+    releaseYear: 2018,
+    director: `Bryan Singer`,
+    starring: [`Rami Malek`, `Lucy Boynton`, `Gwilym Lee`, `Ben Hardy`, `Joseph Mazzello`],
+    runTime: `2h 14m`,
+    ratingScore: `9,0`,
+    ratingLevel: `Very good`,
+    ratingCount: 2477,
+    description: `The story of the legendary British rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Live Aid (1985)`
   },
   {
     title: `Macbeth`,
-    poster: `img/macbeth.jpg`
+    poster: `img/macbeth.jpg`,
+    bgPosterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQysPLT4Y_MGSTpM8VeSqBtUojC0cqdt5KRmL4H1x-PIBhURSQh`,
+    genre: `Dramas`,
+    releaseYear: 2015,
+    director: `Justin Kurzel`,
+    starring: [`Michael Fassbender`, `Marion Cotillard`, `Jack Madigan`, `David Thewlis`,	`Paddy Considine`],
+    runTime: `1h 53m`,
+    ratingScore: `9,0`,
+    ratingLevel: `Very good`,
+    ratingCount: 2477,
+    description: `Macbeth, the Thane of Glamis, receives a prophecy from a trio of witches that one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, Macbeth murders his king and takes the throne for himself`
   },
   {
     title: `Aviator`,
-    poster: `img/aviator.jpg`
+    poster: `img/aviator.jpg`,
+    bgPosterUrl: `https://cs11.pikabu.ru/post_img/big/2019/02/03/8/1549199981191383504.jpg`,
+    genre: `Dramas`,
+    releaseYear: 2004,
+    director: `Martin Scorsese`,
+    starring: [`Leonardo DiCaprio`, `Cate Blanchett`, `Kate Beckinsale`, `Alec Baldwin`, `John C. Reilly`],
+    runTime: `2h 50m`,
+    ratingScore: `7,5`,
+    ratingLevel: `Good`,
+    ratingCount: 1459,
+    description: `A biopic depicting the early years of legendary Director and aviator Howard Hughes' career from the late 1920s to the mid 1940s`
   },
   {
     title: `We need to talk about Kevin`,
-    poster: `img/we-need-to-talk-about-kevin.jpg`
+    poster: `img/we-need-to-talk-about-kevin.jpg`,
+    bgPosterUrl: `https://cs11.pikabu.ru/post_img/big/2019/02/03/8/1549199981191383504.jpg`,
+    genre: `Thrillers`,
+    releaseYear: 2011,
+    director: `Lynne Ramsay`,
+    starring: [`Tilda Swinton`, `John C. Reilly`, `Ezra Miller`, `Jasper Newell`, `Rock Duer`],
+    runTime: `1h 52m`,
+    ratingScore: `7,9`,
+    ratingLevel: `Good`,
+    ratingCount: 1688,
+    description: `Kevin's mother struggles to love her strange child, despite the increasingly dangerous things he says and does as he grows up. But Kevin is just getting started, and his final act will be beyond anything anyone imagined`
   },
   {
     title: `What We Do in the Shadows`,
-    poster: `img/what-we-do-in-the-shadows.jpg`
+    poster: `img/what-we-do-in-the-shadows.jpg`,
+    bgPosterUrl: `https://posmotre.li/images/thumb/3/34/5407168.jpg/300px-5407168.jpg`,
+    genre: `Horror`,
+    releaseYear: 2014,
+    director: `Jemaine Clement`,
+    starring: [`Jemaine Clement`, `Taika Waititi`, `Cori Gonzalez-Macuer`, `Ben Fransham`, `Jonny Brugh`],
+    runTime: `1h 26m`,
+    ratingScore: `7,7`,
+    ratingLevel: `Good`,
+    ratingCount: 1619,
+    description: `Viago, Deacon and Vladislav are vampires who are finding that modern life has them struggling with the mundane - like paying rent, keeping up with the chore wheel, trying to get into nightclubs and overcoming flatmate conflicts.`
   },
   {
-    title: `Revenant`,
-    poster: `img/revenant.jpg`
+    title: `The Revenant`,
+    poster: `img/revenant.jpg`,
+    bgPosterUrl: `https://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2016/01/the-revenant.jpg?itok=3W5SlZrf`,
+    genre: `Dramas`,
+    releaseYear: 2015,
+    director: `Alejandro G. Iñárritu`,
+    starring: [`Leonardo DiCaprio`, `Tom Hardy`, `Will Poulter`, `Domhnall Gleeson`, `Forrest Goodluck`],
+    runTime: `2h 36m`,
+    ratingScore: `8,7`,
+    ratingLevel: `Very good`,
+    ratingCount: 2159,
+    description: `A frontiersman on a fur trading expedition in the 1820s fights for survival after being mauled by a bear and left for dead by members of his own hunting team`
   },
   {
     title: `Johnny English`,
-    poster: `img/johnny-english.jpg`
+    poster: `img/johnny-english.jpg`,
+    bgPosterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPfWdV243AAGFeKH0asJ6rluuMuOD5E-VBvoOlB_g2K35jT6ff`,
+    genre: `Comedies`,
+    releaseYear: 2003,
+    director: `Peter Howitt`,
+    starring: [`Rowan Atkinson`, `John Malkovich`, `Natalie Imbruglia`, `Tasha de Vasconcelos`, `Ben Miller`],
+    runTime: `1h 28m`,
+    ratingScore: `6,2`,
+    ratingLevel: `Good`,
+    ratingCount: 987,
+    description: `After a sudden attack on MI5, Johnny English, Britain's most confident, yet unintelligent spy, becomes Britain's only spy`
   }
 ];
