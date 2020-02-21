0f7bb675700f451890baca321e68bbeb84f467be
Юлия <orlova.yuliia.v@gmail.com>
2020-02-21 13:36:07 +0300

    3.2 Добавляет метод _renderApp, дорабатывает рендеринг MovieDetails при клике

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,29 +4,51 @@ import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
 import MovieDetails from "../movie-details/movie-details.jsx";
 
-const movieTitleClickHandler = () => {};
-
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      selectedFilm: null
+    };
+
+    this._handleTitleClick = this._handleTitleClick.bind(this);
+  }
+
+  _handleTitleClick(film) {
+    this.setState(
+        {selectedFilm: film}
+    );
+  }
+
+  _renderApp() {
+    const {promoTitle, promoGenre, promoYear, films} = this.props;
+    const {selectedFilm} = this.state;
+
+    if (selectedFilm) {
+      return <MovieDetails film={selectedFilm} />;
+    }
+
+    return (
+      <Main
+        promoTitle={promoTitle}
+        promoGenre={promoGenre}
+        promoYear={promoYear}
+        onMovieTitleClick={this._handleTitleClick}
+        films={films} />);
   }
 
   render() {
-    const {promoTitle, promoGenre, promoYear, films} = this.props;
+    const {selectedFilm} = this.state;
 
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Main
-              promoTitle={promoTitle}
-              promoGenre={promoGenre}
-              promoYear={promoYear}
-              onMovieTitleClick={movieTitleClickHandler}
-              films={films} />
+            {this._renderApp()}
           </Route>
           <Route exact path="/dev-film-details">
-            <MovieDetails />
+            {selectedFilm ? <MovieDetails film={selectedFilm}/> : null}
           </Route>
         </Switch>
       </BrowserRouter>
@@ -34,11 +56,12 @@ class App extends PureComponent {
   }
 }
 
+
 App.propTypes = {
   promoTitle: PropTypes.string.isRequired,
   promoGenre: PropTypes.string.isRequired,
   promoYear: PropTypes.number.isRequired,
-  films: PropTypes.array.isRequired
+  films: PropTypes.arrayOf.isRequired
 };
 
 export default App;
