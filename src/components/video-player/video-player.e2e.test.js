76d9bb63a17e4b22ab39de1812d5378032618149
cronon <cronon12@gmail.com>
2020-02-28 20:10:21 +0300

    fix tests for video player
    play is called right after the component instantiates so we have to mock
    play method before we render the component

--- src/components/video-player/video-player.e2e.test.js
+++ src/components/video-player/video-player.e2e.test.js
@@ -15,44 +15,24 @@ const film = {
 
 it(`should play video`, () => {
   window.HTMLMediaElement.prototype.play = () => {};
-
   const {poster, previewUrl} = film;
+  const play = jest.spyOn(window.HTMLMediaElement.prototype, `play`)
   const videoPlayer = mount(
       <VideoPlayer poster={poster} previewUrl={previewUrl} isPlaying={true} />
   );
-
-  const {_videoRef} = videoPlayer.instance();
-
-  const play = jest
-  .spyOn(_videoRef.current, `play`)
-  .mockImplementation(() => {});
-
-  _videoRef.current.play();
-
   expect(play).toHaveBeenCalledTimes(1);
-
-  expect(play).toHaveBeenCalled();
   play.mockRestore();
 });
 
 it(`should pause video`, () => {
   window.HTMLMediaElement.prototype.pause = () => {};
-
+  const pause = jest.spyOn(window.HTMLMediaElement.prototype, `pause`)
   const {poster, previewUrl} = film;
   const videoPlayer = mount(
       <VideoPlayer poster={poster} previewUrl={previewUrl} isPlaying={true} />
   );
-
   const {_videoRef} = videoPlayer.instance();
-
-  const pause = jest
-    .spyOn(_videoRef.current, `pause`)
-    .mockImplementation(() => {});
-
   _videoRef.current.pause();
-
   expect(pause).toHaveBeenCalledTimes(1);
-
-  expect(pause).toHaveBeenCalled();
   pause.mockRestore();
 });
