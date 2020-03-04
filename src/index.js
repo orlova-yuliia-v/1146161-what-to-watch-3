aea4872208ac67595c3f782b5b36af3a1915c6c0
Юлия <orlova.yuliia.v@gmail.com>
2020-03-04 16:46:01 +0300

    5.1 Добавляет подключение Redux DevTools

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const PromoData = {
   YEAR: 2014
 };
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
