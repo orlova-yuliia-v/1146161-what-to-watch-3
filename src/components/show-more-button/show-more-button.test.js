cb2e96401dcb69c10dff7443e66b2962ec0890dd
Юлия <orlova.yuliia.v@gmail.com>
2020-03-07 00:36:24 +0300

    5.2 Добавляет снепшот тест для ShowMoreButton

---
+++ src/components/show-more-button/show-more-button.test.js
@@ -0,0 +1,212 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import ShowMoreButton from "./show-more-button.jsx";
+import {ALL_GENRES} from "../../const.js";
+
+const DEFAULT_SHOWED_MOVIES_NUMBER = 8;
+
+const mockStore = configureStore([]);
+
+const films = [
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    title: `Some title`,
+    poster: `1.jpg`,
+    bgPosterUrl: `https://image-url.com/1.jpg`,
+    genre: `Some genre`,
+    releaseYear: 2020,
+    director: `Director name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`, `Actor 4`, `Actor 5`],
+    runTime: `2h 00m`,
+    ratingScore: 7.5,
+    ratingCount: 1234,
+    description: `Film description`,
+    previewUrl: `https://preview-url.com/1.mp4`,
+    reviews: [
+      {
+        ratingScore: 8.5,
+        date: `September 8, 2019`,
+        author: `Yuliia Orlova`,
+        text: `Review text`
+      }
+    ]
+  }
+];
+
+
+it(`Should render SimilarMovies component`, () => {
+  const store = mockStore({
+    selectedGenre: ALL_GENRES,
+    films,
+    showedMovies: DEFAULT_SHOWED_MOVIES_NUMBER
+  });
+
+  const tree = renderer
+    .create(
+        <Provider store={store}>
+          <ShowMoreButton
+            films={[]}
+            showedMovies={DEFAULT_SHOWED_MOVIES_NUMBER}
+            showMoreMovies={() => {}}
+          />
+        </Provider>
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
