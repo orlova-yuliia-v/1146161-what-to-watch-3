0cbae24765f46d93c88358908a0baeccc18d62f3
Юлия <orlova.yuliia.v@gmail.com>
2020-03-19 11:11:16 +0300

    7.1 Создает модуль API

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+const TIMEOUT = 5000;
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/wtw`,
+    timeout: TIMEOUT,
+    withCredentials: true
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onError = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onError);
+
+  return api;
+};
