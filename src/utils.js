64a8d9cd85ad0fe6a32cf6a8dc9f200eb5ca69b1
Юлия <orlova.yuliia.v@gmail.com>
2020-03-23 11:19:27 +0300

    7.1 Добавляет нормализацию данных и форматирование даты отзыва

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,37 @@
 export const extend = (a, b) => {
   return Object.assign({}, a, b);
 };
+
+export const formatReviewDate = ({comment, date, id, rating, user}) => ({
+  comment,
+  date: new Date(date).toLocaleString(`en-us`, {
+    month: `long`,
+    year: `numeric`,
+    day: `numeric`
+  }),
+  id,
+  rating,
+  user
+});
+
+export const normalizeMovieData = (movie) => ({
+  title: movie.name,
+  poster: movie.poster_image,
+  preview: movie.preview_image,
+  bgPosterUrl: movie.background_image,
+  backgroundColor: movie.background_color,
+  description: movie.description,
+  ratingScore: movie.rating,
+  ratingCount: movie.scores_count,
+  director: movie.director,
+  starring: movie.starring,
+  runTime: movie.run_time,
+  genre: movie.genre,
+  releaseYear: movie.released,
+  id: movie.id,
+  isFavorite: movie.is_favorite,
+  videoUrl: movie.video_link,
+  previewUrl: movie.preview_video_link
+});
+
+export const normalizeMoviesData = (movies) => movies.map(normalizeMovieData);
