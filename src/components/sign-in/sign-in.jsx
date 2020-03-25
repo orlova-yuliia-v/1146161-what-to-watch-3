df4b5982845b575afc5dde30f614574e4dd3e163
Юлия <orlova.yuliia.v@gmail.com>
2020-03-25 11:21:41 +0300

    7.2 Конвертирует компонент SignIn в классовый

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -1,77 +1,110 @@
-import React from "react";
+import React, {PureComponent, createRef} from "react";
+import PropTypes from "prop-types";
 
-const SignIn = () => {
-  return (
-    <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
+class SignIn extends PureComponent {
+  constructor(props) {
+    super(props);
 
-        <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+    this.loginRef = createRef();
+    this.passwordRef = createRef();
 
-      <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input
-                className="sign-in__input"
-                type="email"
-                placeholder="Email address"
-                name="user-email"
-                id="user-email"
-              />
-              <label
-                className="sign-in__label visually-hidden"
-                htmlFor="user-email"
-              >
+    this._handleFormSubmit = this._handleFormSubmit.bind(this);
+  }
+
+  _handleFormSubmit(evt) {
+    const {onSubmit} = this.props;
+
+    evt.preventDefault();
+
+    onSubmit({
+      login: this.loginRef.current.value,
+      password: this.passwordRef.current.value
+    });
+  }
+
+  render() {
+    return (
+      <div className="user-page">
+        <header className="page-header user-page__head">
+          <div className="logo">
+            <a href="main.html" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <h1 className="page-title user-page__title">Sign in</h1>
+        </header>
+
+        <div className="sign-in user-page__content">
+          <form
+            action="#"
+            className="sign-in__form"
+            onSubmit={this._handleFormSubmit}
+          >
+            <div className="sign-in__fields">
+              <div className="sign-in__field">
+                <input
+                  className="sign-in__input"
+                  type="email"
+                  placeholder="Email address"
+                  name="user-email"
+                  id="user-email"
+                  ref={this.loginRef}
+                />
+                <label
+                  className="sign-in__label visually-hidden"
+                  htmlFor="user-email"
+                >
                 Email address
-              </label>
-            </div>
-            <div className="sign-in__field">
-              <input
-                className="sign-in__input"
-                type="password"
-                placeholder="Password"
-                name="user-password"
-                id="user-password"
-              />
-              <label
-                className="sign-in__label visually-hidden"
-                htmlFor="user-password"
-              >
+                </label>
+              </div>
+              <div className="sign-in__field">
+                <input
+                  className="sign-in__input"
+                  type="password"
+                  placeholder="Password"
+                  name="user-password"
+                  id="user-password"
+                  ref={this.passwordRef}
+                />
+                <label
+                  className="sign-in__label visually-hidden"
+                  htmlFor="user-password"
+                >
                 Password
-              </label>
+                </label>
+              </div>
             </div>
-          </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">
+            <div className="sign-in__submit">
+              <button className="sign-in__btn" type="submit">
               Sign in
-            </button>
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
           </div>
-        </form>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
       </div>
+    );
+  }
+}
 
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
-  );
+SignIn.propTypes = {
+  onSubmit: PropTypes.func.isRequired
 };
 
 export default SignIn;
