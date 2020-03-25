f2f727260a19e8c51bc4c7f93b812001933c79d9
Юлия <orlova.yuliia.v@gmail.com>
2020-03-25 11:27:25 +0300

    7.2 Добавляет login в пропсы, упрощает PropTypes для selectedMovie

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -8,6 +8,7 @@ import {ActionCreator} from "../../reducer/state/state.js";
 import {getPromoMovie} from "../../reducer/data/selectors.js";
 import {isFullVideoPlayer, getSelectedMovie} from "../../reducer/state/selectors.js";
 import SignIn from "../sign-in/sign-in.jsx";
+import {Operation as UserOperation} from "../../reducer/user/user.js";
 
 class App extends PureComponent {
   constructor(props) {
@@ -54,7 +55,7 @@ class App extends PureComponent {
             {this._renderApp()}
           </Route>
           <Route exact path="/dev-sign-in">
-            <SignIn />
+            <SignIn onSubmit={this.props.login}/>
           </Route>
           <Route exact path="/dev-film-page">
             {selectedMovie ?
@@ -78,26 +79,9 @@ App.propTypes = {
   onVisibilityChange: PropTypes.func.isRequired,
   isFullVideoPlayerVisible: PropTypes.bool.isRequired,
   getComments: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
   changeSelectedMovieId: PropTypes.func.isRequired,
-  selectedMovie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired,
-    preview: PropTypes.string.isRequired,
-    previewUrl: PropTypes.string.isRequired,
-    bgPosterUrl: PropTypes.string.isRequired,
-    backgroundColor: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    ratingScore: PropTypes.number.isRequired,
-    ratingCount: PropTypes.number.isRequired,
-    director: PropTypes.string.isRequired,
-    starring: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-    runTime: PropTypes.number.isRequired,
-    genre: PropTypes.string.isRequired,
-    releaseYear: PropTypes.number.isRequired,
-    id: PropTypes.number.isRequired,
-    isFavorite: PropTypes.bool.isRequired,
-    videoUrl: PropTypes.string.isRequired,
-  })
+  selectedMovie: PropTypes.shape()
 };
 
 const mapStateToProps = (state) => ({
@@ -112,6 +96,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
   changeSelectedMovieId(id) {
     dispatch(ActionCreator.changeSelectedMovieId(id));
+  },
+  login(authData) {
+    dispatch(UserOperation.login(authData));
   }
 });
 
