e4f789dd33b8fc3c518708f4b05bd42e6ae9ac21
Юлия <orlova.yuliia.v@gmail.com>
2020-03-26 23:22:51 +0300

    7.2 Добавляет селектор для получения данных о пользователе

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -3,3 +3,7 @@ import NameSpace from "../name-space.js";
 export const getAuthorizationStatus = (state) => {
   return state[NameSpace.USER].authorizationStatus;
 };
+
+export const getAuthUser = (state) => {
+  return state[NameSpace.USER].authUserData;
+};
